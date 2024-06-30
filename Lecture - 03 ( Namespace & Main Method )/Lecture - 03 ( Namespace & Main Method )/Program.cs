6b9ad51748b629acc3fd0c80f796ee75062c2676
mayankpal20 <mayankpal5820@gmail.com>
2024-07-01 04:52:11 +0530

    Lecture - 03 ( Namespace & Main Method )

---
+++ Lecture - 03 ( Namespace & Main Method )/Lecture - 03 ( Namespace & Main Method )/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___03___Namespace___Main_Method__
+{
+    internal class Program
+    {
+        //Main Method :- Its an entry point , and we can cannot change name of main method
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Namespace & MainMethod");
+            //ReadLine is to take input from user
+            Console.ReadLine();
+        }
+    }
+}
