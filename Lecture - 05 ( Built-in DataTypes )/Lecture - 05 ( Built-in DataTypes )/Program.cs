805fa52eebac37eb4984bce031a6312daa56ac96
mayankpal20 <mayankpal5820@gmail.com>
2024-07-08 03:52:39 +0530

    Lecture - 05 ( Built-in DataTypes )

---
+++ Lecture - 05 ( Built-in DataTypes )/Lecture - 05 ( Built-in DataTypes )/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___05___Built_in_DataTypes__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10000;
+            Console.WriteLine(num);
+           
+            //Get minvalue of datatype
+            Console.WriteLine(int.MinValue);
+            //Get Maxvalue of daatype
+            Console.WriteLine(int.MaxValue);
+
+            //Boolean which return true or false
+            bool val = true;
+            Console.WriteLine(val);
+
+
+            Console.ReadLine();
+        }
+    }
+}
