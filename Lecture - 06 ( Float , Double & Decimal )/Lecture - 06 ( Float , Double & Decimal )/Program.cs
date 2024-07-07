c67953d2641c7b0930b490680d59af33246529ea
mayankpal20 <mayankpal5820@gmail.com>
2024-07-08 04:04:27 +0530

    Lecture - 06 ( Float , Double & Decimal )

---
+++ Lecture - 06 ( Float , Double & Decimal )/Lecture - 06 ( Float , Double & Decimal )/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___06___Float___Double___Decimal__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Float datatype    
+            float val = 3.3321f;
+            //Double datatype
+            double val1 = 4.232323d;
+            //Long datatype
+            long val2 = 63276372l;
+
+            Console.WriteLine(val1);
+            Console.WriteLine(val);
+            Console.WriteLine(val2);
+
+            Console.ReadLine();
+        }
+    }
+}
