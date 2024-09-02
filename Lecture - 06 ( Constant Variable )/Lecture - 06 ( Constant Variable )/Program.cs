7de39c067c9ef2f90a8a8cc63c90b0d808588d90
mayankpal46 <mayankpal4629@gmail.com>
2024-09-02 10:19:33 -0700

    Lecture - 06 ( Constant Variable )

---
+++ Lecture - 06 ( Constant Variable )/Lecture - 06 ( Constant Variable )/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___06___Constant_Variable__
+{
+    public class Program
+    {
+        //Constant Variable can be declare inside class or inside a method
+        //Here constant variable is public which means it can be access anywhere
+        public const double PI = 3.14;
+        static void Main(string[] args)
+        {
+            //Constant Variable inside a method
+            const int newvariable = 100;
+
+            Console.WriteLine(PI);
+            Console.ReadLine();
+        }
+    }
+}
