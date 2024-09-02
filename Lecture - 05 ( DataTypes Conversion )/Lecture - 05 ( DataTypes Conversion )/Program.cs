6bbd1428cf7fb5880849198a7f402cd17f089c56
mayankpal46 <mayankpal4629@gmail.com>
2024-09-02 09:51:17 -0700

    Lecture - 05 ( DataTypes Conversion )

---
+++ Lecture - 05 ( DataTypes Conversion )/Lecture - 05 ( DataTypes Conversion )/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = 50;
+
+//Implicit Conversion
+float b = a;
+Console.WriteLine(b);
+
+float c = 100f;
+
+//Explicit Conversion
+int d = Convert.ToInt32(c);
+Console.WriteLine(d);
+
+string s1 = "1000";
+string s2 = "1992";
+
+//Explicit Conversion
+int value = Convert.ToInt32(s1) + Convert.ToInt32(s2);
+Console.WriteLine(value);
