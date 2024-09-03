5f9d878fa4e49665e7823ccc3462f62d1c943935
mayankpal46 <mayankpal4629@gmail.com>
2024-09-03 22:38:41 +0530

    Lecture - 08 ( Statements , Expression )

---
+++ Lecture - 08 ( Statements , Expression )/Lecture - 08 ( Statements , Expression )/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___08___Statements___Expression__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Statements :- It makes using some expressions and statement ends with ; 
+            // Statements ------> Expressions -------> Operators , Operands 
+            int i = 100;
+            Console.WriteLine(i);
+
+            int j = 402;
+
+            //Expressions 
+            int sum = i + j ;
+            Console.WriteLine(sum);
+
+
+        }
+    }
+}
