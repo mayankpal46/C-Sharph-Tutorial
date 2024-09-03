21aa3e6a8c643ac3e026c2b043ff39e77e2279c4
mayankpal46 <mayankpal4629@gmail.com>
2024-09-03 22:51:58 +0530

    Lecture - 09 ( Arthmetic Operators )

---
+++ Lecture - 09 ( Arthmetic Operators )/Lecture - 09 ( Arthmetic Operators )/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___09___Arthmetic_Operators__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+            int b = 15;
+
+            Console.WriteLine(a+b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a % b);
+            Console.WriteLine(a / b);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
