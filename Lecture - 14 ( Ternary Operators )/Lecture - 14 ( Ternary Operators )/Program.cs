06ce91598d4899184af552cfdd69e5e7a82349ba
mayankpal46 <mayankpal4629@gmail.com>
2024-09-04 19:47:57 +0530

    Lecture - 14 ( Ternary Operators )

---
+++ Lecture - 14 ( Ternary Operators )/Lecture - 14 ( Ternary Operators )/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___14___Ternary_Operators__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int val = (3 > 4 ? 3 : 0);
+            Console.WriteLine(val);
+
+            bool bval = (true==true?true:false);
+            Console.WriteLine(bval);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
