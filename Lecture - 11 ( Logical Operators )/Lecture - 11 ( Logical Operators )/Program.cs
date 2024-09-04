efe95de3288f939e2b63de8750df0446a5e735fc
mayankpal46 <mayankpal4629@gmail.com>
2024-09-04 19:08:36 +0530

    Lecture - 11 ( Logical Operators )

---
+++ Lecture - 11 ( Logical Operators )/Lecture - 11 ( Logical Operators )/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___11___Logical_Operators__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 219;
+            int num2 = 900;
+
+            //Logical Or Conditional Operator :- && , ||
+
+            Console.WriteLine((num1 > 0) && (num2 <= 900));
+            Console.WriteLine((num1 == 10) && (num2 <= 2));
+            Console.WriteLine((num1 != 1000) && (num2 == 122));
+            Console.WriteLine((num1 > 0) || (num2 <= 900));
+            Console.WriteLine((num1 == 10) || (num2 <= 2));
+            Console.WriteLine((num1 != 1000) || (num2 == 122));
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
