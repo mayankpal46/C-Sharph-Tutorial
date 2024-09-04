80a55981d5913db97c11024846e8198648949df2
mayankpal46 <mayankpal4629@gmail.com>
2024-09-04 19:36:57 +0530

    Lecture - 13 ( Inc or Dec Operators )

---
+++ Lecture - 13 ( Inc or Dec Operators )/Lecture - 13 ( Inc or Dec Operators )/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___13___Inc_or_Dec_Operators__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1002;
+            int m = 213;
+
+            //First assign then increase which is post increment
+            int k = n++;
+            Console.WriteLine(k);
+
+            // Pre Increment
+            int j = ++n;
+            Console.WriteLine(j);
+
+            Console.ReadLine();
+
+
+            
+        }
+    }
+}
