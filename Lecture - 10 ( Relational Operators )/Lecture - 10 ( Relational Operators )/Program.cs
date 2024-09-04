fbc4453a79cd18df0572be28596cc1dee09eca87
mayankpal46 <mayankpal4629@gmail.com>
2024-09-04 18:51:10 +0530

    Lecture - 10 ( Relational Operators )

---
+++ Lecture - 10 ( Relational Operators )/Lecture - 10 ( Relational Operators )/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___10___Relational_Operators__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 199;
+            int b = 213;
+
+            //Relational Operator :- < , > , != , == , >= , <=
+            Console.WriteLine(a<b);
+            Console.WriteLine(a>b);
+            Console.WriteLine(a!=b);
+            Console.WriteLine(a==b);
+            Console.WriteLine(a>=b);
+            Console.WriteLine(a<=b);
+
+            Console.ReadLine(); 
+        }
+    }
+}
