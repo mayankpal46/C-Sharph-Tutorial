15fea74f7bf54492207e530b5ceb6572cd8a5cf6
mayankpal46 <mayankpal4629@gmail.com>
2024-09-04 19:25:39 +0530

    Lecture - 12 ( Assignment Operators )

---
+++ Lecture - 12 ( Assignment Operators )/Lecture - 12 ( Assignment Operators )/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___12___Assignment_Operators__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            //Simple Assignment Operator
+            int num1 = 221;
+            int num2 = num1 + 1;
+
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+
+            //Compound Assignment Operator :- >= , <= , -= , *= , /= , %=
+            Console.WriteLine(num2>=num1);
+            Console.WriteLine(num2 *= num1);
+            Console.WriteLine(num2 -= num1);
+
+
+            Console.ReadLine(); 
+        }
+    }
+}
