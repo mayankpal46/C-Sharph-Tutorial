29f6333affeae392fffdad366bbd6b803e0a4d98
mayankpal46 <mayankpal4629@gmail.com>
2024-09-09 08:25:03 -0700

    Lecture - 19 ( Switch Case )

---
+++ Lecture - 19 ( Switch Case )/Lecture - 19 ( Switch Case )/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___19___Switch_Case__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Switch Case :- To avoid multiple if statements
+
+            int no = 192;
+
+            switch (no) 
+            {
+                case 100:
+                    Console.WriteLine("Isse bada hai no");
+                    break;
+                case 150:
+                    Console.WriteLine("ye bhi chota hai no");
+                    break;
+                case 192:
+                    Console.WriteLine("Mil Gaya");
+                    break;
+                default:
+                    Console.WriteLine("Kuch nahi match hua");
+                    break;
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
