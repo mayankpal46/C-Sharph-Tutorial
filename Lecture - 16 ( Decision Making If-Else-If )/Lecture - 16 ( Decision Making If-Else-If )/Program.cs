c9575f114da8f408726b3595694319529243df5f
mayankpal46 <mayankpal4629@gmail.com>
2024-09-09 07:38:08 -0700

    Lecture - 16 ( Decision Making If-Else-If )

---
+++ Lecture - 16 ( Decision Making If-Else-If )/Lecture - 16 ( Decision Making If-Else-If )/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___16___Decision_Making_If_Else_If__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Decision Making :- If-Else-If
+            int x = 212;
+            if (x == 100)
+            {
+                Console.WriteLine("Mil Gaya");
+            }
+            else if (x >= 212)
+            {
+                Console.WriteLine("match hua hai");
+            }
+            else
+            {
+                Console.WriteLine("Kuch Nahi Mila");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lecture - 16 ( Decision Making If-Else-If )/Lecture - 16 ( Decision Making If-Else-If )/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Lecture - 16 ( Decision Making If-Else-If )")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lecture - 16 ( Decision Making If-Else-If )")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("539256e9-d163-4f38-be11-71f708b4918c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
