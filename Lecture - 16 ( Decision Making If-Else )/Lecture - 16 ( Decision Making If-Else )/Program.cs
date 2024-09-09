040ef28f646b0821b24a9d4938f9ad8256a6f20f
mayankpal46 <mayankpal4629@gmail.com>
2024-09-09 07:16:38 -0700

    Lecture - 16 ( Decision Making If-Else )

---
+++ Lecture - 16 ( Decision Making If-Else )/Lecture - 16 ( Decision Making If-Else )/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___16___Decision_Making_If_Else__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Decision Making :- If-else
+
+            int x = 199;
+            int y = 213;
+
+            if (x >= 221)
+            {
+                Console.WriteLine("It is true");
+            }
+            else 
+            {
+                Console.WriteLine("It is False");
+            }
+
+
+            string name = "Tukonmeikhamakha";
+            string pass = "accha";
+            
+            //Logical And in If-Else
+            if(name== "Tukonmeikhamakha" && pass == "accha")
+            {
+                Console.WriteLine("Permission Granted");
+            }
+            else
+            {
+                Console.WriteLine("Permission Denied");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
