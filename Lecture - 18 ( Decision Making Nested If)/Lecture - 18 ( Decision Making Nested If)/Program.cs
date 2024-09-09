9852fd9cfe5935572f9562b8bfc337bd334b6167
mayankpal46 <mayankpal4629@gmail.com>
2024-09-09 08:01:19 -0700

    Lecture - 18 ( Decision Making Nested If)

---
+++ Lecture - 18 ( Decision Making Nested If)/Lecture - 18 ( Decision Making Nested If)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___18___Decision_Making_Nested_If_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Nested If :- One If statement inside another if statement
+
+            int no = 182; 
+            if (no < 200)
+            {
+                if (no == 182)
+                {
+                    Console.WriteLine("Mil Gaya be");
+                }
+                else
+                {
+                    Console.WriteLine("Nahi Mila");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
