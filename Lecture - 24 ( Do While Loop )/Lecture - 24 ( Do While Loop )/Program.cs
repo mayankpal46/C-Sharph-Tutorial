24e6c9f68bcd654a4fd190a006b784e3c95fc09b
mayankpal46 <mayankpal4629@gmail.com>
2024-09-10 07:27:56 -0700

    Lecture - 24 ( Do While Loop )

---
+++ Lecture - 24 ( Do While Loop )/Lecture - 24 ( Do While Loop )/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___24___Do_While_Loop__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int s = 100;
+            do
+            {
+                Console.WriteLine("First Do-While Loop");
+                s++;
+            }
+            while (s < 10);
+
+            Console.ReadLine();
+            
+        }
+    }
+}
