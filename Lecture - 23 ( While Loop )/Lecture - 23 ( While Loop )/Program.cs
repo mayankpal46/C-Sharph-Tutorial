3ccf9b1180ea84f206db5ae5313d1b35dafe3ec6
mayankpal46 <mayankpal4629@gmail.com>
2024-09-10 07:16:14 -0700

    Lecture - 23 ( While Loop )

---
+++ Lecture - 23 ( While Loop )/Lecture - 23 ( While Loop )/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___23___While_Loop__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //While Loop
+
+            int u = 1;
+            while (u < 12)
+            {
+                Console.WriteLine("Print Karte Ja");
+                u++;
+            }
+            if (u == 12)
+            {
+                Console.WriteLine("Print rok de be yahi pe");
+            }
+            else
+            {
+                Console.WriteLine("Mat rok");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
