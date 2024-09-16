c5709c14c8d67133906cff3b37cd117cc0f14a1a
mayankpal46 <mayankpal4629@gmail.com>
2024-09-16 09:10:38 -0700

    Lecture - 25 ( Diff between For And While Loop )

---
+++ Lecture - 25 ( Diff between For And While Loop )/Lecture - 25 ( Diff between For And While Loop )/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___25___Diff_between_For_And_While_Loop__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //For Loop
+
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("For Loop Ends");
+            Console.WriteLine("\t");
+            //While Loop
+
+            int j = 9;
+            while (j > 0) 
+            {
+                Console.WriteLine(j);
+                j--;
+            }
+
+            Console.WriteLine("While Loop Ends");
+
+            Console.ReadLine();
+        }
+    }
+}
