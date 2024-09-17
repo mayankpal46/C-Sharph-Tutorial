40a1e9a99728b05fe5bbbae39a258465582aec8c
mayankpal46 <mayankpal4629@gmail.com>
2024-09-17 08:43:36 +0530

    Lecture - 29 ( Arrays in C-Sharph )

---
+++ Lecture - 29 ( Arrays in C-Sharph )/Lecture - 29 ( Arrays in C-Sharph )/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___29___Arrays_in_C_Sharph__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Declaring a array 
+
+            //1st Way 
+            // new keyword :- Basically it asign memory
+            int[] newarray = new int[5];
+
+            //Asssigning values using index number
+            newarray[0] = 1;
+            newarray[1] = 2;
+            newarray[2] = 3;
+
+            Console.WriteLine(newarray[2]);
+            Console.WriteLine(newarray[3]);
+
+            Console.WriteLine("\t");
+
+            //2nd Way 
+            string[] newarray2 = new string[] {"name1","name2","name3"};
+
+            //3rd Way
+            string[] newarray3 = {"myname1","myname2","myname3"};
+
+            Console.WriteLine(newarray3[0]);
+            Console.WriteLine(newarray3[1]);
+            Console.WriteLine(newarray3[2]);
+
+            //Getting size of an array
+            Console.WriteLine("Length of an array is : " + newarray3.Length);
+
+
+            Console.ReadLine();
+        }
+    }
+}
