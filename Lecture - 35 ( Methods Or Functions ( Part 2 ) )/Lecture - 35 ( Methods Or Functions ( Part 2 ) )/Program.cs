b816ed4de8507b1ec87497925ed4d5bbd3163ada
mayankpal46 <mayankpal4629@gmail.com>
2024-09-18 15:28:05 +0530

    Lecture - 35 ( Methods Or Functions ( Part 2 ) )

---
+++ Lecture - 35 ( Methods Or Functions ( Part 2 ) )/Lecture - 35 ( Methods Or Functions ( Part 2 ) )/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___35___Methods_Or_Functions___Part_2____
+{
+    internal class Program
+    {
+        public static void M1(string name="Unkown" , int age=10)
+        {
+            Console.WriteLine(name + " : " + age);
+        }
+
+        public static int IntValue(int no)
+        {
+            return no;
+        }
+        static void Main(string[] args)
+        {
+            Program.M1("dev", 20);
+
+            //If location of parameter change
+            Program.M1(age: 21, name: "omkara");
+
+            int num = Program.IntValue(100);
+            Console.WriteLine("Input no is : " + num);
+
+
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
