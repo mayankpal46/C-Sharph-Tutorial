472df5bf9769044729f8540312edf245fd510d6f
mayankpal46 <mayankpal4629@gmail.com>
2024-09-20 09:08:48 +0530

    Lecture - 40 ( Access Specifier ( Part 1 ) )

---
+++ Lecture - 40 ( Access Specifier ( Part 1 ) )/Lecture - 40 ( Access Specifier ( Part 1 ) )/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___40___Access_Specifier___Part_1____
+{
+    public class A
+    {
+        public void M1()
+        {
+            Console.WriteLine("Method 1");
+        }
+
+        public void M3()
+        {
+            A obj1 = new A();
+            obj1.M1();
+        }
+        void M2() 
+        {
+            Console.WriteLine("Private Method");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Class which is public
+             A obj = new A();
+             obj.M1();
+            obj.M3();
+            
+        }
+    }
+}
---
+++ Lecture - 40 ( Access Specifier ( Part 1 ) )/NewNamespace/namsp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lecture___40___Access_Specifier___Part_1____;
+
+namespace NewNamespace
+{
+    
+    class namsp : A
+    {
+        static void Main(string[] args)
+        { 
+            A ob = new A();
+            ob.M1();
+
+            //Inherit parent class which is present in another namespace & then creating object of our child class
+            namsp obs = new namsp();
+            obs.M3();
+        }
+    }
+}
