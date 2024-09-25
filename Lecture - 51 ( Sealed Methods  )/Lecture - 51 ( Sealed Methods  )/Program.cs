f3cb47e2f900afb2dc327d0178c26cea7b265e78
mayankpal46 <mayankpal4629@gmail.com>
2024-09-25 22:47:17 +0530

    Lecture - 51 ( Sealed Methods )

---
+++ Lecture - 51 ( Sealed Methods  )/Lecture - 51 ( Sealed Methods  )/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___51___Sealed_Methods___
+{
+    class A
+    {
+        public virtual void M1()
+        {
+            Console.WriteLine("Method 1");
+        }
+    }
+
+    class B : A
+    {
+        public sealed override void M1()
+        {
+            Console.WriteLine("Method 2");
+        }
+    }
+
+    class C : B
+    {
+        //public override void M1()
+        //{
+        //    Console.WriteLine("Method 3");
+        //}
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            C obj = new C();
+            obj.M1();
+
+            Console.ReadLine();
+        }
+    }
+}
