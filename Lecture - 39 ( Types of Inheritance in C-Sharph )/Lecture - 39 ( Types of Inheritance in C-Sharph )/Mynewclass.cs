b4e0ce296a56f91cbbbb841895a5c10990ef2026
mayankpal46 <mayankpal4629@gmail.com>
2024-09-25 22:26:03 +0530

    Lecture - 50 ( Sealed class  )

--- Lecture - 39 ( Types of Inheritance in C-Sharph )/Lecture - 39 ( Types of Inheritance in C-Sharph )/Mynewclass.cs
+++ Lecture - 39 ( Types of Inheritance in C-Sharph )/Lecture - 39 ( Types of Inheritance in C-Sharph )/Mynewclass.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Lecture___39___Types_of_Inheritance_in_C_Sharph__
 {
-     class Mynewclass : ClassA
+     class Mynewclass : ClassB
     {
         public int Newvar;
     }
---
+++ Lecture - 50 ( Sealed class  )/Lecture - 50 ( Sealed class  )/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___50___Sealed_class___
+{
+    //sealed class A
+    //{
+    //    public void M1()
+    //    {
+    //        Console.WriteLine("Method 1");
+    //    }
+    //}
+
+    class A
+    {
+        public void M1()
+        {
+            Console.WriteLine("Method 1");
+        }
+    }
+    class B : A
+    {
+        public void M2()
+        {
+            Console.WriteLine("Method 2");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            B obj = new B();
+            obj.M1();
+
+            Console.ReadLine();
+        }
+    }
+}
