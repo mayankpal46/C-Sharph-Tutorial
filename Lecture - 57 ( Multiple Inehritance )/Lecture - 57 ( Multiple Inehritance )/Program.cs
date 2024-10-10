f4629a1f2cd030e419314f28641f7b434cdf9d5c
mayankpal46 <mayankpal4629@gmail.com>
2024-10-10 08:40:02 +0530

    Lecture - 57 ( Multiple Inehritance )

---
+++ Lecture - 57 ( Multiple Inehritance )/Lecture - 57 ( Multiple Inehritance )/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___57___Multiple_Inehritance__
+{
+    class A
+    {
+        public void M1()
+        {
+            Console.WriteLine("Method 1");
+        }
+
+        public void M2()
+        {
+            Console.WriteLine("Method of Class A");
+        }
+    }
+
+    interface B
+    {
+        void M2();
+    }
+
+    // Multiple Inheritance
+    class C : A , B
+    {
+        void B.M2()
+        {
+            Console.WriteLine("Method of Interface");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            C obj = new C();
+            obj.M1();
+            obj.M2();
+
+            //Casting :- To call method which is present in interface 
+            ((B)obj).M2();
+        }
+    }
+}
