75bdef5e93f54cb1b7a98a2771e7ac0d8e41fea4
mayankpal46 <mayankpal4629@gmail.com>
2024-10-10 12:48:48 +0530

    Lecture - 59 ( HashTable Part2 )

---
+++ Lecture - 59 ( HashTable Part2 )/Lecture - 59 ( HashTable Part2 )/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___59___HashTable_Part2__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Syntax 1
+            Hashtable hashtable = new Hashtable();
+
+            //Add is just a method
+            hashtable.Add("a", 1);
+            hashtable.Add("b", "Rudra");
+            hashtable.Add(200, "Omkara");
+
+            //Accessing all element through for loop
+            foreach (var key in hashtable.Keys)
+            {
+                Console.WriteLine(hashtable[key]);            
+            }
+            
+            hashtable.Remove("a");
+            Console.WriteLine('\t');
+             
+            Console.WriteLine("After Removing element");
+            //After removing
+            foreach (var key in hashtable.Keys)
+            {
+                Console.WriteLine(hashtable[key]);
+            }
+
+            Console.WriteLine('\t');
+
+        }
+    }
+}
