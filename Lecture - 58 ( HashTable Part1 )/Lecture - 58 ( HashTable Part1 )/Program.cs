9c1a3e06c9d0089a264013024f32331e7b3ca341
mayankpal46 <mayankpal4629@gmail.com>
2024-10-10 12:13:43 +0530

    Lecture - 58 ( HashTable Part1 )

---
+++ Lecture - 58 ( HashTable Part1 )/Lecture - 58 ( HashTable Part1 )/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___58___HashTable_Part1__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Syntax 1
+            Hashtable hashtable = new Hashtable();
+
+            //Add is just a method
+            hashtable.Add("a", 1);
+            hashtable.Add("b", "Rudra");
+
+
+            //Syntax 2
+
+            Hashtable hashtable2 = new Hashtable()
+            {
+                {"d",32},
+                {"f","Omkara"}
+            };
+
+            //Accessing value which store on that particular key
+            Console.WriteLine(hashtable["b"]);
+        }
+    }
+}
---
+++ Lecture - 58 ( HashTable Part1 )/Lecture - 58 ( HashTable Part1 )/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Lecture - 58 ( HashTable Part1 )")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lecture - 58 ( HashTable Part1 )")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("356aa284-ad1f-4c47-af18-a779efac7f94")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
