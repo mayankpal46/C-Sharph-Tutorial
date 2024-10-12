d7d89f633785b56e4a0f87053010f9a7341b830a
mayankpal46 <mayankpal4629@gmail.com>
2024-10-12 11:29:48 +0530

    Lecture - 61 ( Queue Non-Generic )

---
+++ Lecture - 61 ( Queue Non-Generic )/Lecture - 61 ( Queue Non-Generic )/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___61___Queue_Non_Generic__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue myqu = new Queue();
+
+            //Pushing elements in queue
+            myqu.Enqueue(1);
+            myqu.Enqueue("Rudra");
+            myqu.Enqueue(100.10);
+
+            //accessing elements from queue
+
+            foreach (var item in myqu)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Removing element
+            Console.WriteLine(myqu.Dequeue());
+
+        }
+    }
+}
