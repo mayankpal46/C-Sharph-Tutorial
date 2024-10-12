a6c0bbad6aea5b13df6dccba1505d8bfd783b84a
mayankpal46 <mayankpal4629@gmail.com>
2024-10-12 10:45:34 +0530

    Lecture - 60 ( Stack Non-Generic )

---
+++ Lecture - 60 ( Stack Non-Generic )/Lecture - 60 ( Stack Non-Generic )/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___60___Stack_Non_Generic__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack myst = new Stack();
+            
+            //Pushing value in stack
+            myst.Push(1);
+            myst.Push("Rudra");
+            myst.Push(10.101);
+            myst.Push(true);
+
+            //Accessing elemnts of stack
+            foreach(var item in myst)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}
---
+++ Lecture - 60 ( Stack Non-Generic )/Lecture - 60 ( Stack Non-Generic )/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Lecture - 60 ( Stack Non-Generic )")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lecture - 60 ( Stack Non-Generic )")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f80f6528-98c4-4fc3-bceb-675c71cf3ff5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
