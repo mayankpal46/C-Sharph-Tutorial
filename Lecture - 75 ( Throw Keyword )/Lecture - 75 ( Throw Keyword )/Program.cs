e6fbb05896b88b051b13ec01766864d176dd3a8c
mayankpal46 <mayankpal4629@gmail.com>
2024-10-13 18:38:14 +0530

    Lecture - 75 ( Throw Keyword )

---
+++ Lecture - 75 ( Throw Keyword )/Lecture - 75 ( Throw Keyword )/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___75___Throw_Keyword__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+            int b = 0;
+            
+            //Here exception will throw by default and catch by default
+            int c = a / b;
+
+            try
+            {
+                //Here exception will by c# and catch by us
+                c = a / b;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("Number de be");
+            int num = int.Parse(Console.ReadLine());
+            if (num == 100)
+            {
+                Console.WriteLine("Number equal hai");
+            }
+            else
+            {
+                throw new Exception("Number equal nahi hai");
+            }
+
+
+
+
+        }
+    }
+}
