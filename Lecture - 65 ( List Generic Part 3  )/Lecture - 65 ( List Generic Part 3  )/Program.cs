f310c2228fd6f30729fea56ad887e3a685d47b01
mayankpal46 <mayankpal4629@gmail.com>
2024-10-13 11:10:48 +0530

    Lecture - 65 ( List Generic Part 3  )

---
+++ Lecture - 65 ( List Generic Part 3  )/Lecture - 65 ( List Generic Part 3  )/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___65___List_Generic_Part_3___
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> newl = new List<int>();
+            newl.Add(191);
+            newl.Add(89);
+            newl.Add(21);
+
+            Console.WriteLine(newl.Contains(21));
+            Console.WriteLine(newl.Contains(211));
+
+        }
+    }
+}
