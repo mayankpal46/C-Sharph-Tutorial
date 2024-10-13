185c9250946ae47e3cc67baff65cc42e255149c5
mayankpal46 <mayankpal4629@gmail.com>
2024-10-13 17:41:31 +0530

    Lecture - 73 ( Try With Multiple Catch )

---
+++ Lecture - 73 ( Try With Multiple Catch )/Lecture - 73 ( Try With Multiple Catch )/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___73___Try_With_Multiple_Catch__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+            int b = 0;
+            
+
+            try
+            {
+                int c = a / b;
+
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(NullReferenceException ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+        }
+    }
+}
