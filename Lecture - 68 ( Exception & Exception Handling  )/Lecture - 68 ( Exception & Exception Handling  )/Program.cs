ed9f88b636c637b61fcddb92dc250582bb795763
mayankpal46 <mayankpal4629@gmail.com>
2024-10-13 16:38:51 +0530

    Lecture - 68 ( Exception & Exception Handling  )

---
+++ Lecture - 68 ( Exception & Exception Handling  )/Lecture - 68 ( Exception & Exception Handling  )/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___68___Exception___Exception_Handling___
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number");
+            int no1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number");
+            int no2 = int.Parse(Console.ReadLine());
+
+            int num = 0;
+
+            try
+            {
+                num = no1 / no2;
+                Console.WriteLine("Result is : " + num);
+            }
+
+            // ex is a rerfrence variable of class dividebyzeroexception
+            catch(DivideByZeroException ex)
+            {
+                //It will print error message
+                Console.WriteLine(ex.Message);
+                //Console.WriteLine("Number can not divide by zero"); ;
+            }
+
+            
+            Console.WriteLine("Statement 1");
+
+
+            Console.WriteLine('\t');
+        }
+    }
+}
