b45ede44458a811d77d600cf96ee721d8fac6100
mayankpal46 <mayankpal4629@gmail.com>
2024-10-13 17:00:49 +0530

    Lecture - 70 ( Handling Null Refrence )

---
+++ Lecture - 70 ( Handling Null Refrence )/Lecture - 70 ( Handling Null Refrence )/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___70___Handling_Null_Refrence__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Omkara";
+            Console.WriteLine("Length is : " + name.Length);
+
+            string naam = null;
+            try
+            {
+                Console.WriteLine(naam.Length);
+            }
+            catch(NullReferenceException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+
+
+        }
+    }
+}
