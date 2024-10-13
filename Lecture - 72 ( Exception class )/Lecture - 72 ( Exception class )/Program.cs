7dec36cf87cd5400e7a84e0bc6d0e83814ace4cb
mayankpal46 <mayankpal4629@gmail.com>
2024-10-13 17:22:50 +0530

    Lecture - 72 ( Exception class )

---
+++ Lecture - 72 ( Exception class )/Lecture - 72 ( Exception class )/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___72___Exception_class__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = null;
+
+            try
+            {
+                Console.WriteLine(s.Length);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
