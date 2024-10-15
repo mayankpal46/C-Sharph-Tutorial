65ababd3e024a1c52fabfe09fc3d9ddd319bc3c9
mayankpal46 <mayankpal4629@gmail.com>
2024-10-15 12:40:15 +0530

    Lecture - 84 ( Structure Part 1 )

---
+++ Lecture - 84 ( Structure Part 1 )/Lecture - 84 ( Structure Part 1 )/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___84___Structure_Part_1__
+{
+    internal struct Program
+    {
+        public void m1()
+        {
+            Console.WriteLine("Method 1");
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.m1();
+        }
+    }
+}
