bb06618f275d424e400b812944088377ee5a12c1
mayankpal46 <mayankpal4629@gmail.com>
2024-10-15 06:58:09 +0530

    Lecture - 79 ( String And StringBuilder )

---
+++ Lecture - 79 ( String And StringBuilder )/Lecture - 79 ( String And StringBuilder )/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___79___String_And_StringBuilder__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Rudra";
+            string s2 = "Omkara";
+            s1 = s1 + s2;
+            Console.WriteLine(s1);
+
+
+            //StringBuilder
+
+            StringBuilder obj = new StringBuilder("Shivayee");
+            obj.Append(" Oberoi");
+
+            Console.WriteLine(obj);
+        }
+    }
+}
