be294bba1af7df12d48c673fc3700099f198e2e3
mayankpal46 <mayankpal4629@gmail.com>
2024-10-15 06:01:51 +0530

    Lecture - 77 ( Rules For Partial Class )

---
+++ Lecture - 77 ( Rules For Partial Class )/Lecture - 77 ( Rules For Partial Class )/ExamplePartial1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___77___Rules_For_Partial_Class__
+{
+    public partial class c1
+    {
+    }
+}
---
+++ Lecture - 77 ( Rules For Partial Class )/Lecture - 77 ( Rules For Partial Class )/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture___77___Rules_For_Partial_Class__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating object of partial class
+            c1 obj = new c1();
+            obj.m1();
+        }
+    }
+
+    public class mclass
+    {
+        public void m1()
+        {
+            Console.WriteLine("My method");
+        }
+    }
+}
